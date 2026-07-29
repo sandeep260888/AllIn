470aa64f6aa181a052d204862133a8704b54ae6d
sandeep260888 <83490257+sandeep260888@users.noreply.github.com>
2026-07-30 01:18:07 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,21 +1,18 @@
-const CACHE_NAME = 'ledger-pwa-v1';
+const CACHE_NAME = 'ledger-pwa-v2';
 const ASSETS = [
   './',
   './index.html',
-  './manifest.json'
+  './manifest.json',
+  './data.json'
 ];
 
-// Install Event
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
   self.skipWaiting();
 });
 
-// Activate Event
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((keys) => {
@@ -29,7 +26,6 @@ self.addEventListener('activate', (event) => {
   self.clients.claim();
 });
 
-// Fetch Event (Offline-First Capability)
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((cachedResponse) => {
